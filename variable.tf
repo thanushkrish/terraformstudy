6f95781737995828efc4c984bf3f552ad2303487
thanushkrish <112387982+thanushkrish@users.noreply.github.com>
2023-06-27 14:35:20 +0530

    Create variable.tf
    Syntax and Default values.

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "sg_create" {  #name 
+  
+
+  description = "test"   #give a description about vatiable
+  type = number          #type of data
+  default = 22           #if we didn assign value it will consider default
+
+}
